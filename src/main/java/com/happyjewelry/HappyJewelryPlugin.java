ab79cfdfc0c950723dda3e1ec4dd03127b757f1b
Kent Ritchie <kintobean@github.com>
2024-11-26 21:07:52 -0700

    tickle

--- src/main/java/com/happyjewelry/HappyJewelryPlugin.java
+++ src/main/java/com/happyjewelry/HappyJewelryPlugin.java
@@ -1,5 +1,9 @@
 package com.happyjewelry;
 
+import net.runelite.api.events.MenuEntryAdded;
+import net.runelite.api.MenuEntry;
+import net.runelite.api.ChatMessageType;
+import net.runelite.client.chat.ChatMessageManager;
 import com.google.inject.Provides;
 import net.runelite.api.Client;
 import net.runelite.api.events.MenuOptionClicked;
@@ -20,6 +24,9 @@ import java.io.InputStream;
 public class HappyJewelryPlugin extends Plugin {
 
 	@Inject
+	private ChatMessageManager chatMessageManager;
+
+	@Inject
 	private Client client;
 
 	@Inject
@@ -36,8 +43,34 @@ public class HappyJewelryPlugin extends Plugin {
 	}
 
 	@Subscribe
+	public void onMenuEntryAdded(MenuEntryAdded event) {
+		// Check if the menu option is "Rub"
+		if (event.getOption().equals("Rub")) {
+			// Get the menu entries
+			MenuEntry[] entries = client.getMenuEntries();
+
+			// Modify the last menu entry (which corresponds to the event)
+			MenuEntry lastEntry = entries[entries.length - 1];
+			lastEntry.setOption("Tickle"); // Change "Rub" to "Tickle"
+
+			// Set the modified entries back
+			client.setMenuEntries(entries);
+		}
+	}
+
+	private void sendChatMessage(String message) {
+		client.addChatMessage(
+				ChatMessageType.GAMEMESSAGE,
+				"",
+				message,
+				null
+		);
+	}
+
+	@Subscribe
 	public void onMenuOptionClicked(MenuOptionClicked event) {
-		if (event.getMenuOption().equals("Rub")) { // Match "Rub" menu option
+		if (event.getMenuOption().equals("Tickle")) { // Match "Rub" menu option
+			sendChatMessage("That tickles!");
 			if (config.soundEnabled()) {  // Check if sound is enabled in the config
 				playCustomSound();
 			}
